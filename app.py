1cd93216fde911d906405188b2a31cb1512570b7
Alber-Pa <aparot@fi.uba.ar>
2026-09-22 18:08:46 -0300

    Initialize Flask app with CORS and logging
    Set up Flask application with logging and CORS enabled.

---
+++ app.py
@@ -0,0 +1,29 @@
+import logging
+from flask import Flask
+from flask_cors import CORS
+from constantes import BASE_URL 
+from src.route.deportes import deportes_bp
+#from src.route.canchas import canchas_bp
+#from src.route.socios import socios_bp
+#from src.route.reservas import reservas_bp
+#from src.route.extensiones import extensiones_bp
+
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(name)s - %(message)s')
+
+app = Flask(__name__)
+app.json.sort_keys = False
+
+# Habilitar CORS para que el frontend pueda consumir la API
+CORS(app)
+
+app.register_blueprint(deportes_bp, url_prefix=BASE_URL)
+#app.register_blueprint(canchas_bp, url_prefix=BASE_URL)
+#app.register_blueprint(socios_bp, url_prefix=BASE_URL)
+#app.register_blueprint(reservas_bp, url_prefix=BASE_URL)
+#app.register_blueprint(extensiones_bp, url_prefix=BASE_URL)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
