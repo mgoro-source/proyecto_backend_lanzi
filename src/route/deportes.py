e192bcd7fc2eb5091e5ccedb4ac613f17d11061f
Alber-Pa <aparot@fi.uba.ar>
2026-09-22 18:22:10 -0300

    Add Flask route to list sports

--- src/route/deportes.py
+++ src/route/deportes.py
@@ -1,1 +1,17 @@
+from flask import Blueprint, jsonify, request
+from ..services.deportes import (
+    listar_deportes,
+    )
+
+deportes_bp = Blueprint('deportes', __name__)
+
+
+@deportes_bp.route('/deportes', methods=['GET'])
+def get_deportes():
+    deportes = listar_deportes()
+
+    if not deportes:
+        return '', 204
+
+    return jsonify(deportes)
 
