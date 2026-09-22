db87b1b3bcf74429b82b6fe0c1a9cfe700f352ba
Alber-Pa <aparot@fi.uba.ar>
2026-09-22 18:21:11 -0300

    Update deportes.py

--- src/services/deportes.py
+++ src/services/deportes.py
@@ -1,1 +1,17 @@
+import logging
+import db
 
+logger = logging.getLogger(__name__)
+
+def construir_deporte(deporte: dict) -> dict:
+    
+    return {
+        'id_deporte':   deporte['id_deporte'],
+        'nombre_deporte':   deporte['nombre_deporte'],
+        
+    }
+
+
+def listar_deportes() -> list[dict]:
+    
+    return [construir_deporte(a) for a in db.obtener_todos_los_deportes()]
